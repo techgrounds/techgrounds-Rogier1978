0dd461ce218fc2589e2ae5bf558c83a11c2de9f1
Rogier1978 <129735945+Rogier1978@users.noreply.github.com>
2023-07-19 23:07:38 +0200

    Delete 000_Project/version1.1 deliverables directory

--- 000_Project/version1.1 deliverables/customdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-    sudo su
-    apt update
-    apt install apache2 -y
-    ufw allow 'Apache'
-    systemctl enable apache2
-    systemctl restart apache2
-    sudo apt install mysql-client-core-8.0
-    sudo apt-get install mysql-server
